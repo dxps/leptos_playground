8476259a800a547e5b0c196ad76a4b2c979802a0
dxps <marius.ileana@gmail.com>
2025-07-07 17:14:02 +0300

    feat: navbar links
    Shown when logged in. No permissions check yet.

--- user_dir_lap/src/ui/comps/navbar.rs
+++ user_dir_lap/src/ui/comps/navbar.rs
@@ -2,7 +2,7 @@ use crate::ui::{
     comps::NavUserMenu,
     logic::get_current_user,
     state::{UiState, UiStateStoreFields},
-    styles,
+    styles::{self, NAVBAR_LIST, NAVBAR_LIST_ITEM},
 };
 use leptos::prelude::*;
 use leptos::{logging::log, reactive::spawn_local};
@@ -42,26 +42,41 @@ pub fn Navbar() -> impl IntoView {
 
     view! {
         <nav class="absolute w-full px-4 py-1 flex justify-between items-center bg-white z-40">
-        <a href="/" class="py-1.5 hover:bg-white">
-            <img src="/img/favicon/favicon-32x32.png" class="w-[24px] h-[24px]"/>
-        </a>
-        <ul
-            class="hidden absolute top-1/2 sm:left-1/3 sm:pl-16 md:left-1/2 lg:left-1/2
-                    transform -translate-y-1/2 -translate-x-1/2"
-        >
-            <li>
-                <a href="/" class="text-sm text-gray-600 py-1 px-4 hover:bg-gray-100 rounded-lg transition duration-200">Home</a>
-            </li>
-        </ul>
-        <Show
-        when=move || state.is_logged_in().get()
-        fallback=|| view!{
-            <a href="/login"
-               class=styles::NAVBAR_LINK
-               >Login</a>
-        }>
-           <NavUserMenu/>
-        </Show>
-    </nav>
+            <a href="/" class="py-1.5 hover:bg-white">
+                <img src="/img/favicon/favicon-32x32.png" class="w-[24px] h-[24px]"/>
+            </a>
+            <Show
+                when=move || state.is_logged_in().get()
+                fallback=|| view!{
+                    <a href="/login"
+                    class=styles::NAVBAR_LINK
+                    >Login</a>
+                }>
+                <ul class=NAVBAR_LIST>
+                    <li>
+                        <a href="/" class=NAVBAR_LIST_ITEM>Home</a>
+                    </li>
+                    <li class="text-gray-400">
+                        <NavSep/>
+                    </li>
+                    <li>
+                        <a href="/accounts" class=NAVBAR_LIST_ITEM>Accounts</a>
+                    </li>
+                </ul>
+                <NavUserMenu/>
+            </Show>
+        </nav>
+    }
+}
+
+#[component]
+pub fn NavSep() -> impl IntoView {
+    view! {
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"
+            class="w-4 h-4 current-fill" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+            />
+        </svg>
     }
 }
--- user_dir_lap/src/ui/styles/mod.rs
+++ user_dir_lap/src/ui/styles/mod.rs
@@ -1,6 +1,11 @@
 pub const TITLE_CSS: &str = "flex-grow text-lg font-medium px-2 text-center text-gray-400";
 
-pub const NAVBAR_LINK: &str = "text-sm text-gray-600 py-1 px-4 hover:bg-gray-100 rounded-lg transition duration-200 sm:inline-block sm:ml-auto sm:mr-3";
+pub const NAVBAR_LINK: &str = "text-sm text-gray-600 py-1 px-4 hover:bg-gray-200 rounded-lg transition duration-200 sm:inline-block sm:ml-auto sm:mr-3";
+
+pub const NAVBAR_LIST: &str = "absolute top-1/2 xs:hidden sm:left-1/3 sm:pl-16 md:left-1/2 lg:left-1/2 transform -translate-y-1/2 -translate-x-1/2 sm:flex sm:mx-auto sm:flex sm:items-center sm:w-auto sm:space-x-3 lg:space-x-6";
+
+pub const NAVBAR_LIST_ITEM: &str =
+    "text-sm text-gray-600 py-1 px-4 rounded-lg transition duration-200";
 
 pub const USER_MENU_LINK: &str = "py-2.5 px-7 min-w-full w-max min-h-full flex text-[#333]";
 
